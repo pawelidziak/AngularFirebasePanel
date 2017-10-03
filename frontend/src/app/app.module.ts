f895ab69381170ee673e830b876612ca9b96ee3f
Paweł Idziak <pa.idziak@gmail.com>
2017-10-03 11:37:11 +0200

    Dodanie AngularFire2 4.0

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,14 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpModule } from "@angular/http";
 import { NgModule } from '@angular/core';
 
+import { AngularFireModule } from 'angularfire2';
+
 import { AppComponent } from './app.component';
+
+// Initialize Firebase
+export const firebaseConfig = {
+  apiKey: "AIzaSyBNWs6hRtJLwfMT3tHkL9w7HIScwJ0d2WM",
+  authDomain: "angularfirebasepanel.firebaseapp.com",
+  databaseURL: "https://angularfirebasepanel.firebaseio.com",
+  projectId: "angularfirebasepanel",
+  storageBucket: "angularfirebasepanel.appspot.com",
+  messagingSenderId: "512213003997"
+};
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
